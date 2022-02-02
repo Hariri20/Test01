fd7ea138b5a3c8a5fae06ba10533351a73f4b320
Hariri20 <72672199+Hariri20@users.noreply.github.com>
2022-02-02 15:52:05 +1100

    change all to remote driver

--- src/test/java/com/accesshq/test/WebTestSuit/BaseTest.java
+++ src/test/java/com/accesshq/test/WebTestSuit/BaseTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest
 {
@@ -14,12 +17,20 @@ public class BaseTest
     @BeforeEach
     public void Setup() throws MalformedURLException
     {
+        /*
         driver = new ChromeDriver();
         //System.setProperty("webdriver.chrome.driver", "C:\\IT\\chromedriver_win32\\chromedriver.exe");
         // go to the website
         driver.get("https://d18u5zoaatmpxx.cloudfront.net/");
         driver.manage().window().maximize();
+    */
+        var chromeOptions = new ChromeOptions();
+        driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"),chromeOptions);
+
+        driver.get("https://d18u5zoaatmpxx.cloudfront.net/");
+        driver.manage().window().maximize();
     }
+
 
     @AfterEach
     public void cleanup()
